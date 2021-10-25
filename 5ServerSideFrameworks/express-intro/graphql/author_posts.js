5a8a05de1bbca49fdc2cdf0cff514f9499a7f879
Leon Freudenthaler <leon.freudenthaler@stud.fh-campuswien.ac.at>
2021-10-25 15:23:01 +0200

    added graphql and rest to express backend

---
+++ 5ServerSideFrameworks/express-intro/graphql/author_posts.js
@@ -0,0 +1,33 @@
+
+const db = require('../database/database');
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    extend type Query {
+        posts: [Post]
+        post(id: ID!): Post
+    }
+    type Post {
+        id: ID!
+        title: String
+        content: String
+        author: Author
+    }
+`
+
+const resolvers = {
+    Query: {
+        posts: async () => db.posts.findAll(),
+        post: async (obj, args, context, info) =>
+            db.posts.findByPk(args.id),
+    },
+    Post: {
+      author: async (obj, args, context, info) =>
+        db.authors.findByPk(obj.author_id),
+    }
+}
+
+module.exports = {
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+};
